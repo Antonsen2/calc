620e5a9a803c4e71513e208f40b75acfd4dcf6c8
Markus Antonsen <markus-antonsen@hotmail.com>
2021-09-30 11:15:56 +0200

    made a square function

--- Calc.py
+++ Calc.py
@@ -1,20 +1,34 @@
 def plus(a, b):
     return a + b
 
+
 def mult(a, b):
     return a * b
-  
+
+
 def div(a, b):
     return a/b
-  
-  def sub(a, b):
+
+
+def sub(a, b):
     return a - b
 
+
+def rem(a, b):
+    return a % b
+
+
+def square(a, b):
+    return a ** b
+
+
 def main():
+
     print(plus(4, 7))
     print(mult(10, 11))
     print(sub(50, 34))
     print(div(4000, 100))
+    print(square(3, 2))
 
 
 if __name__ == '__main__':
